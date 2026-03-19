7bf233dbeba02fd078b112f7c1707b1d6225dba8
matteoleonardis <16738037+matteoleonardis@users.noreply.github.com>
2026-03-19 16:21:22 +0100

    Sort brands alphabetically
    Added sorting functionality for brands.

--- Model/ResourceModel/Shopbrand/Collection.php
+++ Model/ResourceModel/Shopbrand/Collection.php
@@ -6,17 +6,33 @@
  * @license     http://www.magiccart.net/license-agreement.html
  * @Author: DOng NGuyen<nguyen@dvn.com>
  * @@Create Date: 2016-01-11 23:15:05
- * @@Modify Date: 2016-02-02 15:52:06
- * @@Function:
+ * @@Modify Date: 2026-03-19 16:20:06
+ * @@Function:Sort brands
  */
 
 namespace Magiccart\Shopbrand\Model\ResourceModel\Shopbrand;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
-
     protected function _construct()
     {
-        $this->_init('Magiccart\Shopbrand\Model\Shopbrand', 'Magiccart\Shopbrand\Model\ResourceModel\Shopbrand');
+        $this->_init(
+            'Magiccart\Shopbrand\Model\Shopbrand',
+            'Magiccart\Shopbrand\Model\ResourceModel\Shopbrand'
+        );
+    }
+
+    /**
+     * Sort brands alphabetically by name (A→Z)
+     */
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+
+        // If the name field is called "name", leave it as it is;
+        // if it has a different name (e.g. "title"), change it here.
+        $this->setOrder('name', 'ASC');
+
+        return $this;
     }
 }
